34b370fd538daf4232c46f1e77702d4f2c22e0be
SnehasishRoy <r.snehasish1990@gmail.com>
2024-05-07 17:55:48 -0400

    Create math and if else.py

---
+++ math and if else.py
@@ -0,0 +1,28 @@
+#first_name = "Snehasish"
+#last_name = "Roy"
+#z = first_name + " " +last_name
+#print ("Hello " +z)
+#age = 21
+#print ("age :" +str(age))
+#name = "Snehasish Roy"
+#print ("Your name is "+str(name))
+#import math
+#x = 3.14
+#print (math.ceil(x))
+#print (math.floor(x))
+#print (name*3)
+#website1 =  "https://google.com"
+#slice = slice (8,-4)
+#print (website1[slice])
+#name = input("what is your name: ")
+#print ("Hello " +name)
+age = int(input("what is your age: "))
+if age >=18:
+    print ("you are an adult")
+    if age >= 100:
+        print ("and you are a legend")
+elif age <=0:
+    print ("you are not born yet")
+else:
+    print ("Yor are minor")
+
