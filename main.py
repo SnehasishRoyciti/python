b7e184ec2b1a1d989d11afc3043917726c6a9c2e
SnehasishRoy <r.snehasish1990@gmail.com>
2024-05-06 21:19:49 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
-first_name = "Snehasish"
-last_name = "Roy"
-z = first_name + " " +last_name
-print ("hello " +z)
+#first_name = "Snehasish"
+#last_name = "Roy"
+#z = first_name + " " +last_name
+#print ("Hello " +z)
+#age = 21
+#print ("age :" +str(age))
+#name = "Snehasish Roy"
+#print ("Your name is "+str(name))
+#import math
+#x = 3.14
+#print (math.ceil(x))
+#print (math.floor(x))
+#print (name*3)
+website1 =  "https://google.com"
+slice = slice (8,-4)
+print (website1[slice])
