4fc486c7b2093440c0621efb695620edf656abf1
SnehasishRoy <r.snehasish1990@gmail.com>
2024-05-07 18:07:22 -0400

    Update L2_Logical_Operator.py

---
+++ L2_Logical_Operator.py
@@ -0,0 +1,17 @@
+#temp = int(input ("please entire the temp outside: "))
+#if temp >= 0 and temp <= 30:
+#    print ("its temps is awesome")
+#elif temp < 0 or temp >30:
+#    print ("stay home and drink")
+
+#temp = int(input ("please entire the temp outside: "))
+#if not temp >= 0 and temp <= 30:
+#    print ("its temps is awesome")
+#elif not temp < 0 or temp >30:
+#    print ("stay home and drink")
+
+#name = ""
+#while len(name)==0:
+#    name = input ("Enter your name: ")
+#print ("Hello " +name)
+
